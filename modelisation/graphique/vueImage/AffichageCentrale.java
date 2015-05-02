c4a26ce376e72a5b45b092dd610e5689e04018e9
AnthonM <anthony290991@hotmail.com>
2015-05-02 18:15:06 +0200

    Update AffichageCentrale.java

--- modelisation/graphique/vueImage/AffichageCentrale.java
+++ modelisation/graphique/vueImage/AffichageCentrale.java
@@ -10,6 +10,10 @@ public class AffichageCentrale extends JPanel{
 	
 	protected VueImage vm;
 	
+	/**
+	 * Constructeur AffichageCentrale
+	 * @param m
+	 */
 	public AffichageCentrale(Modele m) {
 		super(new BorderLayout());
 		this.setPreferredSize(new Dimension(200,200));
@@ -17,6 +21,9 @@ public class AffichageCentrale extends JPanel{
 		this.add(vm,BorderLayout.CENTER);
 	}
 	
+	/**
+	 * Initialisation de vm
+	 */
 	public void changement() {
 		vm.setLocation(((this.getWidth())-vm.getWidth())/2,(this.getHeight()-vm.getHeight())/2);
 	}
