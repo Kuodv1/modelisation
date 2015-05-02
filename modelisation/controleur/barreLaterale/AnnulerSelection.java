8f449d25580fbf6d84d994b6732c7efeaa0e5447
AnthonM <anthony290991@hotmail.com>
2015-05-02 18:57:41 +0200

    Update AnnulerSelection.java

--- modelisation/controleur/barreLaterale/AnnulerSelection.java
+++ modelisation/controleur/barreLaterale/AnnulerSelection.java
@@ -9,11 +9,19 @@ public class AnnulerSelection implements ActionListener{
 
 	protected Modele m;
 	
+	/**
+	 * Construteur AnnulerSelection
+	 * @param m
+	 */
 	public AnnulerSelection(Modele m){
 		this.m = m;
 	}
 	
 	@Override
+	/**
+	 * Fonction appelé quand une action se produit
+	 * @param e
+	 */
 	public void actionPerformed(ActionEvent e) {
 		m.annulerSelection();
 		
