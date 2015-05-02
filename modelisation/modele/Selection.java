f6705b8eb7efa5544f234def51722c07e0ac12e2
AnthonM <anthony290991@hotmail.com>
2015-05-02 18:04:51 +0200

    Update Selection.java

--- modelisation/modele/Selection.java
+++ modelisation/modele/Selection.java
@@ -5,11 +5,18 @@ public class Selection {
 	protected int[][] select;
 	protected Selection pere;
 	
+	/**
+	 * Constucteur Selection
+	 */
 	public Selection(int height, int width) {
 		select = new int[height][width];
 		pere = null;
 	}
 	
+	/**
+	 * Constucteur Selection
+	 * @param pere
+	 */
 	public Selection(Selection pere) {
 		select = new int[pere.getHauteur()][pere.getLargeur()];
 		for(int i = 0; i<pere.getHauteur();i++) {
@@ -20,39 +27,78 @@ public class Selection {
 		
 	}
 	
+	/**
+	 * Retourne le tableau select
+	 * @return
+	 */
 	public int[][] getSelect() {
 		return select;
 	}
 	
+	/**
+	 * Initialise la variable select
+	 * @param select
+	 */
 	public void setSelect(int[][] select) {
 		this.select = select;
 	}
 	
+	/**
+	 * Retourne la hauteur du tableau select
+	 * @return
+	 */
 	public int getHauteur() {
 		return select.length;
 	}
 	
+	/**
+	 * Retourne la largeur du tableau select
+	 * @return
+	 */
 	public int getLargeur() {
 		return select[0].length;
 	}
 	
+	/**
+	 * Retourne la selection principale que l'utilisateur à selectionné
+	 * @return
+	 */
 	public Selection getPere() {
 		if(pere!=null) return pere;
 		else return this;
 	}
 	
+	/**
+	 * Retourne un booleen pour annuler la selection de l'utilisateur
+	 * @return
+	 */
 	public boolean annulerSelectPossible() {
 		return pere!=null;
 	}
 	
+	/**
+	 * Retourne la valeur d'une case du tableau select
+	 * @param ligne
+	 * @param colonne
+	 * @return
+	 */
 	public int getValue(int ligne, int colonne) {
 		return select[ligne][colonne];
 	}
 	
+	/**
+	 * Initialise une case du tableau select
+	 * @param ligne
+	 * @param colonne
+	 * @param newValue
+	 */
 	public void setValue(int ligne, int colonne, int newValue) {
 		select[ligne][colonne] = newValue;
 	}
 	
+	/**
+	 * Permet la rotation gauche par l'intermediaire d'un nouveau tableau
+	 */
 	public void rotationGauche() {
 			int[][] newSelect = new int[getLargeur()][getHauteur()];
 			
@@ -65,6 +111,9 @@ public class Selection {
 			this.select = newSelect;
 	}
 	
+	/**
+	 * Permet la rotation droite par l'intermediaire d'un nouveau tableau
+	 */
 	public void rotationDroite() {
 			int[][] newSelect = new int[getLargeur()][getHauteur()];
 			for(int i = 0; i<newSelect.length;i++) {
@@ -75,6 +124,11 @@ public class Selection {
 			this.select = newSelect;
 	}
 	
+	/**
+	 * Effectue une supression de pixel sur la zone selectionnée par l'utilisateur
+	 * @param x
+	 * @param y
+	 */
 	public void supprimerSelection(int x, int y) {
 		int aSuppr = select[y][x];
 		int xDep = x;
@@ -123,34 +177,82 @@ public class Selection {
 		x = xDep;
 	}
 	
+	/**
+	 * Retourne un booleen selon la valeur de x
+	 * @param x
+	 * @param y
+	 * @return
+	 */
 	public boolean possibleGauche(int x, int y) {
 		return x>0;
 	}
 	
+	/**
+	 * Retourne un booleen selon la valeur de y
+	 * @param x
+	 * @param y
+	 * @return
+	 */
 	public boolean possibleBasGauche(int x, int y) {
 		return y+1<getHauteur()&& x>0;
 	}
 	
+	/**
+	 * Retourne un booleen selon la valeur de y et de la hauteur du tableau select
+	 * @param x
+	 * @param y
+	 * @return
+	 */
 	public boolean possibleBas(int x, int y) {
 		return y+1<getHauteur();
 	}
 	
+	 /**
+	 * Retourne un booleen selon la valeur de y et x et de la hauteur et de la largeur du tableau select
+	 * @param x
+	 * @param y
+	 * @return
+	 */
 	public boolean possibleBasDroite(int x, int y) {
 		return y+1<getHauteur()&& x+1<getLargeur();
 	}
 	
+	/**
+	 * Retourne un booleen selon la valeur de x et de la largeur du tableau select
+	 * @param x
+	 * @param y
+	 * @return
+	 */
 	public boolean possibleDroite(int x, int y) {
 		return x+1<getLargeur();
 	}
 	
+	/**
+	 * Retourne un booleen selon la valeur de y et x avec la largeur du tableau select
+	 * @param x
+	 * @param y
+	 * @return
+	 */
 	public boolean possibleHautDroite(int x, int y) {
 		return y>0&&x+1<getLargeur();
 	}
 	
+	/**
+	 * Retourne un booleen selon la valeur de y
+	 * @param x
+	 * @param y
+	 * @return
+	 */
 	public boolean possibleHaut(int x, int y) {
 		return y>0;
 	}
 	
+	/**
+	 * Retourne un booleen selon la valeur de y et x 
+	 * @param x
+	 * @param y
+	 * @return
+	 */
 	public boolean possibleHautGauche(int x, int y) {
 		return y>0 && x>0;
 	}
