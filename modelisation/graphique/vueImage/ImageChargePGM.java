6d3b1d8dc44eb5c2a9b0216ed5740db0b1e11db2
Kuod <louismougin54@gmail.com>
2015-05-02 13:27:06 +0200

    modifié:         Partie_Theorique 	nouveau fichier: Partie_Theorique~ 	modifié:         modelisation/graphique/vueImage/ImageChargePGM.java

--- modelisation/graphique/vueImage/ImageChargePGM.java
+++ modelisation/graphique/vueImage/ImageChargePGM.java
@@ -6,37 +6,33 @@ import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.awt.image.ColorModel;
 import java.awt.image.MemoryImageSource;
+import java.awt.image.WritableRaster;
 
 import javax.swing.JPanel;
 
 import modelisation.modele.Modele;
 
 public class ImageChargePGM extends ImageCharge {
-
-	protected Image img;
 	
 	public ImageChargePGM(int width, int height) {
 		super(width,height,BufferedImage.TYPE_BYTE_GRAY);
-		img = null;
 	}
 	
 	
 	public void dessiner(Modele m) {
 		int[][] recup = m.getImage();
+		WritableRaster wr = this.getRaster();
 		ColorModel cm = this.getColorModel();
 		int index = 0;
-		byte[]pix = new byte[recup.length * recup[0].length];
 		for(int y = 0; y<recup.length;y++) {
 			for(int x =  0; x < recup[y].length;x++) {
-				pix[index++] = new Byte((byte) recup[y][x]);
+				wr.setSample(x, y, 0, recup[y][x]);
 			}
 		}
-		JPanel test = new JPanel(); 
-		img = test.createImage(new MemoryImageSource(recup[0].length, recup.length,cm,pix,0,recup[0].length));
-		
+		this.setData(wr);
 	}
 	
 	public Image getImage() {
-		return img;
+		return this;
 	}
 }
